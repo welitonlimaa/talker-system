8bd87043f5b46c1fbb5e9ab7a9fc0aeb119f685b
welitonlimaa <welitonlima159@gmail.com>
2023-02-06 15:19:32 -0300

    Req 2

--- src/routers/talkerRouters.js
+++ src/routers/talkerRouters.js
@@ -12,4 +12,16 @@ router.get('/', async (_req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const talkers = await readTalkerFile();
+  const personTalker = talkers.find((talker) => talker.id === Number(id));
+  if (personTalker) {
+    res.status(200).json(personTalker);
+  } else {
+    res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+});
+
 module.exports = router;
